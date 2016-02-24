d44b6f665e8be262df7ec91cacc1bbe87a0a4ef2
osreboot <calvin.16.weaver@gmail.com>
2016-02-23 20:34:57 -0800

    Pickup system motors.

--- src/org/usfirst/frc/team3268/robot/OI.java
+++ src/org/usfirst/frc/team3268/robot/OI.java
@@ -1,6 +1,8 @@
 package org.usfirst.frc.team3268.robot;
 
 import org.usfirst.frc.team3268.robot.commands.drive.ReverseDrive;
+import org.usfirst.frc.team3268.robot.commands.pickup.SpitCommand;
+import org.usfirst.frc.team3268.robot.commands.pickup.SuckCommand;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
@@ -9,9 +11,13 @@ public class OI {
 	public final Joystick lStick = new Joystick(0), rStick = new Joystick(1);
 	
     private final JoystickButton toggleDriveButton = new JoystickButton(rStick, 2);
+    private final JoystickButton suckButton = new JoystickButton(rStick, 5), spitButton = new JoystickButton(rStick, 3);
     
     public OI() {
     	toggleDriveButton.toggleWhenPressed(new ReverseDrive());
+    	
+    	suckButton.whileHeld(new SuckCommand());
+    	spitButton.whileHeld(new SpitCommand());
     }
 }
 
--- src/org/usfirst/frc/team3268/robot/RobotMap.java
+++ src/org/usfirst/frc/team3268/robot/RobotMap.java
@@ -1,7 +1,10 @@
 package org.usfirst.frc.team3268.robot;
 
 import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.Talon;
 
 public class RobotMap {
     public static final CANTalon leftDrive = new CANTalon(1), rightDrive = new CANTalon(2);
+    
+    public static final Talon intake = new Talon(2);
 }
