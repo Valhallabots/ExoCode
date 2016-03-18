b22ddd2df60ef6f15372a138811e0fc3e360dbf9
osreboot <calvin.16.weaver@gmail.com>
2016-03-17 18:19:51 -0700

    Slight changes.

--- src/org/usfirst/frc/team3268/robot/RobotMap.java
+++ src/org/usfirst/frc/team3268/robot/RobotMap.java
@@ -10,14 +10,16 @@ import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.interfaces.Gyro;
 
 public class RobotMap {
-    public static final CANTalon leftDrive = new CANTalon(1), rightDrive = new CANTalon(2);
-    public static final Talon leftDrive2 = new Talon(5), rightDrive2 = new Talon(2);
+//    public static final CANTalon leftDrive = new CANTalon(1), rightDrive = new CANTalon(2);
+//    public static final Talon leftDrive2 = new Talon(5), rightDrive2 = new Talon(2);
+    
+    public static final Talon leftDrive = new Talon(0), leftDrive2 = new Talon(1), rightDrive = new Talon(2), rightDrive2 = new Talon(3);
     
     public static final DoubleSolenoid solenoid = new DoubleSolenoid(7, 6);
     
-    public static final Talon intake = new Talon(3);
-    public static final Talon firingRight = new Talon(0), firingLeft = new Talon(1);
-    public static final Servo firingServo = new Servo(4);
+    public static final Talon intake = new Talon(6);
+    public static final Talon firingRight = new Talon(4), firingLeft = new Talon(7);
+    public static final Servo firingServo = new Servo(5);
     
     public static final Gyro gyro = new AnalogGyro(1);
 }
